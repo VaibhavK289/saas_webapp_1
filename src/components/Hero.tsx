619a1cc95e4eddf391983aa3185f4748146114e0
VaibhavK <vaibhavkrkandhway@gmail.com>
2025-04-13 19:57:41 +0530

    hero component complete

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,31 @@
+import ArrowWIcon from '../assets/icons/arrow-w.svg';
+
 export const Hero = () => {
-  return null;
+  return (
+    <div className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_84%)] py-[72px]">
+      <div className="container">
+        <div className="flex items-center justify-center">
+          <a
+            href="#"
+            className="inline-flex gap-3 border py-1 px-2 rounded-lg border-white/30"
+          >
+            <span className="bg-[linear-gradient(to_right,#F87AFF,#F893D0_25%,#FFDD99_50%,#C3F0B2_75%,#2FD8FE_100%)] text-transparent bg-clip-text [-webkit-background-clip-text]">
+              Version 2.0 is here
+            </span>
+            <span className="inline-flex items-center gap-1">
+              <span>Read More</span>
+              <ArrowWIcon />
+            </span>
+          </a>
+        </div>
+        <h1 className='text-7xl font-bold tracking-tighter text-center'>One task at a Time</h1>
+        <p className='text-center text-xl mt-8'>
+          Celebrate the joy of accomplishment with an app designed to track your
+          progress, motivate your efforts, and celebrate your successes.
+        </p>
+        <div className='flex justify-center mt-8'></div>
+        <button className='bg-white text-black rounded-lg py-3 px-5 font-medium'>Get for free</button>
+      </div>
+    </div>
+  );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,7 +19,7 @@ export const Navbar = () => {
           <MenuIcon className="text-white" />
         </div>
         <nav className="text-white gap-6 items-center hidden sm:flex">
-          <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition-opacity-100 transition">About</a>
+          <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">About</a>
           <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">Features</a>
           <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">Updates</a>
           <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">Help</a>
