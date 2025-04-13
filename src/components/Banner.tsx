8367cb29320899995546d9d52d2d35007fa5292b
VaibhavK <vaibhavkrkandhway@gmail.com>
2025-04-13 18:46:34 +0530

    first commit and navbar complete

---
+++ src/components/Banner.tsx
@@ -0,0 +1,14 @@
+export const Banner = () => {
+  return (
+    <div className="py-3 text-center bg-[linear-gradient(to_right,rgb(252_214_255_/_0.7),rgb(41_216_255_/_0.7),rgb(255_253_128_/_0.7),rgb(248_154_191_/_0.7),rgb(252_214_255_/_0.7))]">
+      <div className="container">
+        <p className="font-medium">
+          <span className="hidden sm:inline">Introducing a completely redesigned interface-</span>
+          <a href="#" className="underline underline-offset-4">
+            Explore the demo
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import logoImage from '../assets/images/logosaas.png';
+import MenuIcon from '../assets/icons/menu.svg';
+
+export const Navbar = () => {
+  return (
+    <div className=" bg-black">
+    <div className="container">
+      <div className="py-4 flex item-center justify-between">
+        <div className="relative">
+          <div className="absolute w-full top-2 bottom-0 bg-[linear-gradient(to_right,#FCD6FF,#29D8FF,#FFFD80,#F89ABF,#FCD6FF)] blur-md"></div>
+          <Image
+            src={logoImage}
+            alt="Saas logo"
+            className="h-12 w-12 relative"
+          />
+        </div>
+        <div className="border border-white border-opacity-30 h-10 w-10 inline-flex justify-center items-center rounded-lg sm:hidden">
+          <MenuIcon className="text-white" />
+        </div>
+        <nav className="text-white gap-6 items-center hidden sm:flex">
+          <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition-opacity-100 transition">About</a>
+          <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">Features</a>
+          <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">Updates</a>
+          <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">Help</a>
+          <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">Customers</a>
+          <button className="bg-white py-2 px-4 text-black rounded-lg">Get for free</button>
+        </nav>
+      </div>
+    </div>
+  </div>
+  );
+};
